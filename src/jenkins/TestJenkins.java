00c18e36b2fd54fcf7c0b2af941d05e28f5cc73e
Priyanka Mishra <priyankamishra8m@gmail.com>
2016-08-13 18:39:20 +0530

    aaaa

--- src/jenkins/TestJenkins.java
+++ src/jenkins/TestJenkins.java
@@ -5,16 +5,43 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class TestJenkins 
 {
+	ExtentReports e=new ExtentReports("./ExtentReport/ExtentReports.html");
+	
 	@Test
 	public void testingJenkins()
 	{
-		WebDriver driver=new FirefoxDriver();
-		driver.get("http://www.google.com");
-		System.out.println("Successfully opened!");
-		driver.close();
-		System.out.println("Successfully Closed!");
+		ExtentTest t = e.startTest("testingJenkins");
+		t.log(LogStatus.PASS, "Hi...Bye");
+		e.endTest(t);
+		//e.flush();
+		
+//		WebDriver driver=new FirefoxDriver();
+//		driver.get("http://www.google.com");
+//		System.out.println("Successfully opened!");
+//		driver.close();
+//		System.out.println("Successfully Closed!");
 	
+	}
+	
+	@Test
+	public void testingJenkinsAsFail()
+	{
+		
+		ExtentTest t = e.startTest("testingJenkinsAsFail");
+		t.log(LogStatus.FAIL, "GoodMorning....Good Night");
+		e.endTest(t);
+		e.flush();
+		
+//		WebDriver driver=new FirefoxDriver();
+//		driver.get("http://www.facebook.com");
+//		System.out.println("Successfully opened!");
+//		driver.close();
+//		System.out.println("Successfully Closed!");
 	}
 }
